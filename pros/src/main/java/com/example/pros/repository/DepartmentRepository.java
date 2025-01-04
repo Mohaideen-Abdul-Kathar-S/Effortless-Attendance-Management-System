e042ff9eb408cf41117790a0fc7e56ba60da6fab
Mohaideen Abdul Kathar S <157392970+Mohaideen-Abdul-Kathar-S@users.noreply.github.com>
2025-01-04 09:21:25 +0530

    Delete pros directory

--- pros/src/main/java/com/example/pros/repository/DepartmentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.pros.repository;
-
-import com.example.pros.model.Department;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface DepartmentRepository extends MongoRepository<Department, String> {
-}
--- pros/src/main/java/com/example/pros/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.pros.repository;
-
-import com.example.pros.model.User;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface UserRepository extends MongoRepository<User, String> {
-    User findByUsername(String username);
-}
